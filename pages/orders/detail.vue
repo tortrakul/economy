f36c7804c064221235f79950a010661dabab3e05
tortrakul <tortrakul.c@hotmail.com>
2021-08-27 12:46:12 +0700

    update order detail

---
+++ pages/orders/detail.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <OrderCard name="#21" />
+    </div>
+</template>
+
+<script>
+import OrderCard from '~/components/OrderCard';
+
+export default {
+    components: {
+        OrderCard
+    }
+}
+</script>
