3f25141d438713f2dfc1b58e29ef3fa6b46b6d0a
tortrakul <tortrakul.c@hotmail.com>
2021-08-27 11:20:11 +0700

    update farmers

---
+++ components/FarmerCard.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="card card-body p-2 shadow-sm">
+
+        <img src="~/assets/img/farmer.jpg" class="rounded">
+
+        <div class="p-2">
+            <p class="d-flex align-items-center mb-2">
+              <Farmer class="mr-1" style="width: 16px; height: 16px;" />
+
+              <span class="text-truncate">ชื่อเกษตรกร นามสกุล</span>
+            </p>
+            <small class="d-flex align-items-center text-muted">
+              <Phone class="mr-1" style="width: 16px; height: 16px;" /> 088 888 8888
+            </small>
+        </div>
+    </div>
+</template>
+
+<script>
+import Farmer from '~/components/icons/Farmer';
+import Phone from '~/components/icons/Phone';
+
+export default {
+    components: {
+        Farmer,
+        Phone
+    }
+}
+</script>
--- pages/farmers/index.vue
+++ pages/farmers/index.vue
@@ -1,121 +1,60 @@
 <template>
-  <section class="container tile">
-    <h1>รายชื่อเกษตรกร</h1>
-    <div class="container search">
-      <div class="title">
-        <button>
-          <a href>
-            <i class="material-icons">add</i>เพิ่มรายการ
-          </a>
-        </button>
-      </div>
-      <div class="content">
-        <img src="/svg/search.svg">
-        <input placeholder="ค้นหา">
-      </div>
-      <div class="content-filter">
-        <div class="filter"></div>
-      </div>
+    <div>
+        <div class="card card-body mb-3">
+            <h5 class="card-title">รายชื่อเกษตรกร</h5>
+
+            <div class="d-flex mb-5">
+                <button class="btn btn-gradient-primary pr-3 text-nowrap">
+                    <i class="material-icons">add</i> เพิ่มเกษตรกร
+                </button>
+
+                <div class="input-group mx-3">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text"><i class="material-icons">search</i></span>
+                    </div>
+                    <input type="text" class="form-control" placeholder="ค้นหา" />
+                </div>
+
+                <button class="btn btn-default">
+                    <span class="material-icons-outlined">tune</span>
+                </button>
+            </div>
+
+            <div class="row">
+                <div class="col-md-6 col-lg-3">
+                    <div class="position-relative">
+                        <FarmerCard />
+
+                        <nuxt-link to="/farms/detail" class="stretched-link" />
+                    </div>
+                </div>
+
+                <div class="col-md-6 col-lg-3">
+                    <div class="position-relative">
+                        <FarmerCard />
+
+                        <nuxt-link to="/farms/detail" class="stretched-link" />
+                    </div>
+                </div>
+
+                <div class="col-md-6 col-lg-3">
+                    <div class="position-relative">
+                        <FarmerCard />
+
+                        <nuxt-link to="/farms/detail" class="stretched-link" />
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-    <nuxt-link to="/farmers/detail">
-      <div class="customer-lists">
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-      </div>
-    </nuxt-link>
-  </section>
 </template>
 
 <script>
-import Card from "~/components/cardFarmer";
+import FarmerCard from '~/components/FarmerCard';
 
 export default {
-  components: {
-    Card
-  }
-};
+    components: {
+        FarmerCard
+    }
+}
 </script>
-
-<style lang="scss" scoped>
-@import "@/assets/scss/_variables.scss";
-a {
-  text-decoration: none;
-  color: $dark;
-}
-.container {
-  .customer-lists {
-    display: flex;
-    flex-wrap: wrap;
-  }
-  .search {
-    display: flex;
-    button {
-      width: 140px;
-      height: 40px;
-      border-radius: 30px;
-      border: none;
-      background-image: linear-gradient(to right, #6ac85b, #dbed02);
-      font-family: "Noto Sans Thai", sans-serif;
-      margin: 8px;
-      i {
-        vertical-align: middle;
-      }
-      a {
-        color: $white;
-        text-decoration: none;
-      }
-    }
-    .content {
-      padding: 0;
-      position: relative;
-      input {
-        width: 100%;
-        height: 40px;
-        border-radius: 30px;
-        border: 0;
-        font-family: "Noto Sans Thai", sans-serif;
-        margin: 8px 0;
-        box-shadow: 0px 2px 7px rgba(0, 0, 0, 0.2);
-        padding: 10px;
-        padding-left: 30px;
-        outline: none;
-      }
-      img {
-        position: absolute;
-        vertical-align: middle;
-        margin: 0px;
-        width: 15px;
-        height: 15px;
-        top: 37.7%;
-        left: 1.5%;
-        &:hover {
-        }
-      }
-    }
-    .content-filter {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      width: 67px;
-      .filter {
-        mask: url(/svg/Filter.svg) no-repeat;
-        width: 22px;
-        height: 22px;
-        background-color: $dark;
-        &:hover {
-          background-color: $primary;
-        }
-      }
-    }
-  }
-}
-</style>
