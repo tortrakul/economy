0108be24ac77744775c49d6762c11b75deba7a98
tortrakul <tortrakul.c@hotmail.com>
2021-09-02 05:26:17 +0700

    update account summary

--- components/AccountSummary.vue
+++ components/AccountSummary.vue
@@ -1,5 +1,58 @@
 <template>
     <div class="card card-body">
-        Account Summary
+        <div class="row">
+            <div class="col-md-4 d-flex flex-column">
+                <h6 class="text-center">จำนวนเกษตรกร</h6>
+
+                <div class="card card-body mb-5 justify-content-center flex-grow-1 bg-warning text-center">
+                    <h2>900</h2>
+                    <h5 class="mb-0">คน</h5>
+                </div>
+            </div>
+
+            <div class="col-md-4 d-flex flex-column">
+                <h6 class="text-center">จำนวนสวน</h6>
+
+                <div class="card card-body mb-5 justify-content-center flex-grow-1 bg-blue text-center">
+                    <h2>1,200</h2>
+                    <h5 class="mb-0">สวน</h5>
+                </div>
+            </div>
+
+            <div class="col-md-4 d-flex flex-column">
+                <h6 class="text-center">จำนวนเฉลี่ยสวนต่อเกษตรกร</h6>
+
+                <div class="card card-body mb-5 justify-content-center flex-grow-1 bg-warning text-center">
+                    <h2 class="mb-0">2.34</h2>
+                </div>
+            </div>
+
+            <div class="col-md-4 d-flex flex-column">
+                <h6 class="text-center">รายรับรวมเกษตรกร เดือนสิงหาคม</h6>
+
+                <div class="card card-body justify-content-center flex-grow-1 bg-warning text-center">
+                    <h2>900,xxx</h2>
+                    <h5 class="mb-0">บาท</h5>
+                </div>
+            </div>
+
+            <div class="col-md-4 d-flex flex-column">
+                <h6 class="text-center">รายจ่ายรวมเกษตรกร เดือนสิงหาคม</h6>
+
+                <div class="card card-body justify-content-center flex-grow-1 bg-warning text-center">
+                    <h2>100,xxx</h2>
+                    <h5 class="mb-0">บาท</h5>
+                </div>
+            </div>
+
+            <div class="col-md-4 d-flex flex-column">
+                <h6 class="text-center">รายรับ - รายจ่ายรวมปี 2564</h6>
+
+                <div class="card card-body justify-content-center flex-grow-1 bg-warning text-center">
+                    <h2>100,xxx</h2>
+                    <h5 class="mb-0">บาท</h5>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
